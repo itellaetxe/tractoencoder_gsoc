bcb780f248e33243c78144388be9c4369f58d648
itellaetxe <inigotellaetxe@gmail.com>
2024-06-04 11:53:16 +0200

    [ENH] Adds model specification log and training parameter CLI arguments

--- scripts/train_ae_model_trk.py
+++ scripts/train_ae_model_trk.py
@@ -12,44 +12,70 @@ def process_arguments() -> argparse.Namespace:
     parser.add_argument("--input_trk", type=str, help="Path to the input tractogram (.trk file)")
     parser.add_argument("--input_anat", type=str, help="Path to the input anatomical image (NIfTI file)")
     parser.add_argument("--output_model", type=str, help="Path to the output model")
+    parser.add_argument("--batch_size", type=int, default=20, help="Batch size for training the model")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs for training the model")
+    parser.add_argument("--latent_space_dims", type=int, default=32, help="Number of dimensions in the latent space")
+    parser.add_argument("--kernel_size", type=int, default=3, help="Size of the kernel for the convolutional layers")
     args = parser.parse_args()
+
+    # Sanity check of CLI arguments
     if not os.path.exists(args.input_trk):
-        raise FileNotFoundError(f"Input dataset not found at {args.input_dataset}")
+        raise FileNotFoundError(f"Input dataset not found at {args.input_trk}")
     elif not os.path.exists(args.input_anat):
         raise FileNotFoundError(f"Input anatomical image not found at {args.input_anat}")
     elif not os.path.exists(args.output_model):
         print(f"WARNING: Output model not found at {args.output_model}")
         try:
-            os.mkdir(args.output_model)
+            os.makedirs(args.output_model)
         except (FileNotFoundError, FileExistsError) as e:
             if e.__class__ == FileNotFoundError:
                 print(f"WARNING: Output model directory not found at {args.output_model}")
                 print("INFO: Creating the output directory")
                 os.makedirs(args.output_model)
             elif e.__class__ == FileExistsError:
-                print(f"WARNING: Output model directory not found at {args.output_model}")
-    elif os.path.isdir(args.output_model):
-        print(f"The output_model is not a directory. Your model will be stored at {os.path.join(args.output_model, 'model.weights.h5')}")
-        args.output_model = os.path.join(args.output_model, "model.weights.h5")
+                print(f"WARNING: Output model directory found at {args.output_model}")
+        finally:
+            if os.path.isdir(args.output_model):
+                print(f"INFO: The output_model is a directory. Your model will be stored at {os.path.join(args.output_model, 'model.weights.h5')}")
+                args.output_model = os.path.join(args.output_model, "model.weights.h5")
 
     return args
 
 
+def write_model_specs(spec_file: str) -> None:
+    if not os.path.exists(os.path.dirname(spec_file)):
+        os.makedirs(os.path.dirname(spec_file))
+
+    with open(spec_file, "w") as f:
+        f.write(f"### Model: {model.model.name}\n\n")
+        f.write("### Training parameters:\n")
+        f.write(f"## Batch Size: {args.batch_size}\n")
+        f.write(f"## Epochs: {args.epochs}\n\n")
+        f.write("### Model Parameters:\n")
+        f.write(f"## Latent Space Dimensions: {model.latent_space_dims}\n")
+        f.write(f"## Kernel Size: {model.kernel_size}\n\n")
+        f.write("### Model Architecture:\n")
+        for weight in model.model.weights:
+            f.write(f"## Layer: {weight.path}\n")
+
+    print(f"INFO: Model specs written to {spec_file}.")
+
+    return None
+
+
 if __name__ == "__main__":
-    # Example of using the model
-    latent_space_dims = 32
-    kernel_size = 3
-    model = ae_model.IncrFeatStridedConvFCUpsampReflectPadAE(latent_space_dims=latent_space_dims,
-                                                             kernel_size=kernel_size)
 
     # Get input arguments
     args = process_arguments()
 
+    # Example of using the model
+    model = ae_model.IncrFeatStridedConvFCUpsampReflectPadAE(latent_space_dims=args.latent_space_dims,
+                                                             kernel_size=args.kernel_size)
     # Read the data
     input_data = np.array(utils.read_data(args.input_trk, args.input_anat))
 
     # Train the model
-    model.fit(x=input_data, y=input_data, epochs=10, batch_size=100)
+    model.fit(x=input_data, y=input_data, epochs=args.epochs, batch_size=args.batch_size)
 
     # Save the model
     model.save_weights(args.output_model)
@@ -59,6 +85,12 @@ if __name__ == "__main__":
 
     # Save the tractogram
     trk_fname = os.path.join(os.path.dirname(args.output_model), "output.trk")
+    print(f"INFO: Saving the reconstructed tractogram at {trk_fname}.")
     utils.save_tractogram(streamlines=y,
                           img_fname=args.input_anat,
                           tractogram_fname=trk_fname)
+
+    # Write the model specs to a file for future reference
+    spec_file = os.path.join(os.path.dirname(args.output_model), "model_specs.txt")
+
+    write_model_specs(spec_file)
